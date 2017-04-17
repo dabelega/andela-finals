edb4aad7660cda018f83f24234c9115462e3fcfe
DHANI ABELEGA <shani@DHANI-ABELEGAs-MacBook-Pro.local>
2017-04-17 22:38:01 +0100

    Ejs set up successfully

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,10 @@
 //create a new express router
 const express = require('express'),
-	router = express.Router();
+	router = express.Router(),
+	mainController = require('./controllers/main.controller');
 
 //export this router so we can require it in server.js
 module.exports = router;
 
 //define routes
-router.get('/', (req, res) => {
-	res.send('Home page of Andela Finals App');
-
-});
+router.get('/', mainController.showHome);
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 //grab all dependencies
 const express = require('express'),
 	app = express(),
-	port = process.env.PORT || 8080;
+	port = process.env.PORT || 8080,
+	expressLayouts = require('express-ejs-layouts');
 
 //configure our app
+//tell express where to look for static assets
+app.use(express.static(__dirname + '/public'));
+
+//set ejs as templating
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 //set the routes
-app.use(require('./app/routes'))
+app.use(require('./app/routes'));
 
 //test our server
 app.listen(port, () => {
